d48a884b07708897fcce1bc4a9d09af556e813eb
maxousql <maxlaiyio@outlook.fr>
2023-07-06 15:58:40 +0200

    update controller user

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,15 +2,10 @@ exports.getUsers = (req, res, next) => {
     res.status(200).json({
         posts: [
             {
-                _id: '1',
+                id: '1',
                 name: 'LAI YIO LAI TONG',
                 firstname: 'Maxime',
-                content: 'Utilisateur de base',
-                ecole: 'ESGI',
-                creator: {
-                    name: 'Sciences-u'
-                },
-                createdAt: new Date()
+
             }
         ]
     });
@@ -19,8 +14,9 @@ exports.getUsers = (req, res, next) => {
 exports.createUser = (req, res, next) => {
     const name = req.body.name;
     const firstname = req.body.firstname;
+    const id = req.body.id;
     res.status(201).json({
-        message: 'Post created successfully!',
-        user: { id: new Date().toISOString(), name: name, firstname: contfirstnameent }
+        message: 'Utilisateur ajouté ! GG',
+        user: { id: id, name: name, firstname: firstname }
     });
 };
